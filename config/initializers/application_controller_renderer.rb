59ee2bcbab2ec78988244f7288aec12f3b5eb5ee
134yusuke <124737137+134yusuke@users.noreply.github.com>
2023-03-22 14:48:04 +0000

    showファイルを修正した

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '576f39586b90d08b31fd70d037f740935ec91c3bc2b27110cda8efbe2e02e1ef8e82e2a15b6f1d99efdf3eafd21d1abf26b998e8d26479e7851a0a72b1a50d50'
